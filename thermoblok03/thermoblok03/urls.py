e1f59100ef8b845973ec43de8708dd06ecdc4e3f
MineevHome <i@mineevburyat.ru>
2024-02-15 14:27:39 +0800

    sitemap and robots

--- thermoblok03/thermoblok03/urls.py
+++ thermoblok03/thermoblok03/urls.py
@@ -19,12 +19,21 @@ from django.urls import path, include
 from django.views.generic.base import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.sitemaps.views import sitemap
+from django.contrib.sitemaps import GenericSitemap
 
+from products.models import Products
+from .sitemaps import StaticViewSitemap
 
+sitemaps = {
+    "products": GenericSitemap({"queryset": Products.objects.all()}, priority=0.8),
+    "static": StaticViewSitemap
+}
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('robots.txt', TemplateView.as_view(template_name='robots.txt', content_type="text/plain")),
+    path('sitemap.xml', sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
     path('', TemplateView.as_view(template_name='index.html'), name='index'),
     path('feedback', include('feedback.urls', namespace='feedback')),
     path('instructions/', include('instructions.urls', namespace='instructions')),
